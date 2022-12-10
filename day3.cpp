84ee31d14ce67c5c5cc7db4539f056ae9d5d305a
Shaelyn Tolkamp <tolkamps1@uvic.ca>
2022-12-10 11:50:46 -0800

    Added Day 3 part2.

--- day3.cpp
+++ day3.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <set>
 
 using namespace std;
+
+#define NUM_OF_LETTERS 52
 
 int getPriority(char c){
     if(isupper(c)){
@@ -15,9 +18,12 @@ int main(){
     ifstream infile;
     string line;
     int missplaced_priorities = 0;
+    int group_priorities = 0;
+    int group = 1;
+    int group_letters[NUM_OF_LETTERS+1] = {};
     infile.open("day3input.txt", ios::in);
     while(getline(infile, line)){
-        int letters[53] = {};
+        int letters[NUM_OF_LETTERS+1] = {};
         int x = 0;
         while(x < line.length()){
             int p = getPriority(line[x]);
@@ -30,8 +36,31 @@ int main(){
             }
             x++;
         }
+        //Part 2
+        set<int> line_letters;
+        int letter = 0;
+        while(letter < line.length()){
+            line_letters.insert(getPriority(line[letter]));
+            letter++;
+        }
+        for(auto& num: line_letters){
+            group_letters[num] += 1;
+        }
+        group++;
+        if(group > 3){
+            group = 1;
+            for(int x = 1; x < NUM_OF_LETTERS+1; x++){
+                if(group_letters[x] >= 3){
+                    group_priorities += x;
+                    break;
+                }
+            }
+            std::fill_n(group_letters, 53, 0);
+        }
+        
     }
     infile.close();
     cout << "Part 1: Total missplaced priorities: " << missplaced_priorities << "\n";
+    cout << "Part 2: Total priorities for each group: " << group_priorities << "\n";
     return 0;
 }
